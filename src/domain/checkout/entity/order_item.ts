f22c56225c24293d88ba1dabf104c521ac6a05fd
Maciel <joao.maciel@docnix.com.br>
2023-10-04 22:15:06 -0300

    aplicando clean code

--- src/domain/checkout/entity/order_item.ts
+++ src/domain/checkout/entity/order_item.ts
@@ -1,47 +1,13 @@
 export default class OrderItem {
-  private _id: string;
-  private _productId: string;
-  private _name: string;
-  private _price: number;
-  private _quantity: number;
-  private _total: number;
-
   constructor(
-    id: string,
-    name: string,
-    price: number,
-    productId: string,
-    quantity: number
-  ) {
-    this._id = id;
-    this._name = name;
-    this._price = price;
-    this._productId = productId;
-    this._quantity = quantity;
-    this._total = this.total();
-  }
+      public id: string,
+      public name: string,
+      public price: number,
+      public productId: string,
+      public quantity: number
+  ) {}
 
-  get id(): string {
-    return this._id;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  get productId(): string {
-    return this._productId;
-  }
-
-  get quantity(): number {
-    return this._quantity;
-  }
-
-  get price(): number {
-    return this._price;
-  }
-
-  total(): number {
-    return this._price * this._quantity
+  get total(): number {
+    return this.price * this.quantity;
   }
 }
